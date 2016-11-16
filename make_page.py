7cbd763e63b6ec41ec2fd5ec83270a9c69943f93
Estevan Adrian Pequeno <steven@pequeno.in>
2016-11-16 17:01:27 -0600

    remove ref to public ip for elb instance

--- make_page.py
+++ make_page.py
@@ -3,7 +3,6 @@ import requests
 base_url = 'http://169.254.169.254/latest/meta-data/{}'
 
 instance_id = requests.get(base_url.format('instance-id')).content
-public_ip = requests.get(base_url.format('public-ipv4')).content
 
 page = """\
 <html>
@@ -13,7 +12,7 @@ page = """\
     <body>
         <p>My instance id is: {}</p>
     </body>
-</html>""".format(instance_id, public_ip)
+</html>""".format(instance_id)
 
 with open('/var/www/html/index.html', 'w') as fd:
     fd.write(page)
