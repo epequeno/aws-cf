9fc8371b2e2e149b9df04c69c01a5ae409fbfcb4
Estevan Adrian Pequeno <steven@pequeno.in>
2016-11-17 21:29:55 -0600

    update script and script references

--- scripts/make_page.py
+++ scripts/make_page.py
@@ -1,25 +1,25 @@
 from requests import get
 from datetime import datetime
 
-base_url = 'http://169.254.169.254/latest/meta-data/{}'
-
-instance_id = get(base_url.format('instance-id')).content
-az = get(base_url.format('placement/availability-zone')).content
-now = str(datetime.now())
-
 page = """\
 <html>
     <head>
         <title>My instance info</title>
     </head>
     <body>
-        <p>My instance id is: {id}</p>
-        <p>I am in AZ: {az}</p>
+        <p>My instance id is: {my_id}</p>
+        <p>I am in AZ: {my_az}</p>
         <p>File created at: {now}</p>
     </body>
 </html>"""
 
-page = page.format(id=instance_id, az=az, now=now)
+base_url = 'http://169.254.169.254/latest/meta-data/{}'
+
+instance_id = get(base_url.format('instance-id')).content
+az = get(base_url.format('placement/availability-zone')).content
+now = str(datetime.now())
+
+page = page.format(my_id=instance_id, my_az=az, now=now)
 
 with open('/var/www/html/index.html', 'w') as fd:
     fd.write(page)
