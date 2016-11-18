a083778dbd45a6e1d2c98e5339365a6000afe966
Estevan Adrian Pequeno <steven@pequeno.in>
2016-11-17 19:51:51 -0600

    simple autoscaling example works, update make_page

--- make_page.py
+++ make_page.py
@@ -1,8 +1,11 @@
-import requests
+from requests import get
+from datetime import datetime
 
 base_url = 'http://169.254.169.254/latest/meta-data/{}'
 
-instance_id = requests.get(base_url.format('instance-id')).content
+instance_id = get(base_url.format('instance-id')).content
+az = get(base_url.format('placement/availability-zone')).content
+now = str(datetime.now())
 
 page = """\
 <html>
@@ -10,9 +13,13 @@ page = """\
         <title>My instance info</title>
     </head>
     <body>
-        <p>My instance id is: {}</p>
+        <p>My instance id is: {id}</p>
+        <p>I am in AZ: {az}</p>
+        <p>File created at: {now}</p>
     </body>
-</html>""".format(instance_id)
+</html>"""
+
+page = page.format(id=instance_id, az=az, now=now)
 
 with open('/var/www/html/index.html', 'w') as fd:
     fd.write(page)
